0f271544de3fd176135bf3287eda05120e76d6b0
Pascal Hauser <pascal.hauser457@gmail.com>
2019-12-02 13:37:07 +0100

    new version

--- JavaProject/src/main/java/BloomFilter.java
+++ JavaProject/src/main/java/BloomFilter.java
@@ -1,25 +1,49 @@
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BloomFilter {
-    List<String> strings;
-    String path;
-    //Read in Files
-    Scanner in = new Scanner(System.in);
-    path = String.valueOf(in.nextLine());
-    FileReader reader = new FileReader();
-    BufferedReader inBuffer = new BufferedReader(reader);
+    private List<String> Words = new ArrayList<>();     //stores all the words
+    private String path;                                //path for words.txt
+    private Byte[] mBitArray = new Byte[100];           // Initial Byte Array with length 100
+    private int seed = 1996;                            //Seed for HashFunction
 
-    String line = inBuffer.readLine();
-
-   while (line != null)
-    {
-        System.out.println(line);
-        line = inBuffer.readLine();
+    // ****************************CREATE A NEW BLOOM FILTER WITH WORDS FROM A EXTERNAL SOURCE ***********************************************************
+    public BloomFilter(String path) throws IOException {
+        this.path = path;
+        FileReader reader = new FileReader(path);
+        BufferedReader inBuffer = new BufferedReader(reader);
+        String line = inBuffer.readLine();
+        while (line != null) {
+            Words.add(line);
+            line = inBuffer.readLine();
+        }
     }
-}
+    //Hash all the words in List
+    public void HashAllWords(){
+        HashFunction hf = Hashing.murmur3_32(seed);
+        for(String s : Words){
+            HashCode code = hf.hashUnencodedChars(s);
+            int cod = code.asInt();
+            System.out.println(cod);
+        }
+
+    }
+//Helper function to print all Words in List Words
+    public void print() {
+        for (String s : Words) {
+            System.out.println(s);
+        }
+    }
 
 
 }
--- JavaProject/src/main/java/main.java
+++ JavaProject/src/main/java/main.java
@@ -1,6 +1,10 @@
-public class main {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class main {
+    public static void main(String[] args) throws IOException {
+    BloomFilter bloom = new BloomFilter("TestData/words.txt");
+    bloom.print();
+    bloom.HashAllWords();
     }
     }
 
