8bb3be334e0efd15b6d9cc9bdd9e63fab224a7a0
Pascal Hauser <pascal.hauser457@gmail.com>
2019-12-02 14:41:01 +0100

    new version

--- JavaProject/src/main/java/BloomFilter.java
+++ JavaProject/src/main/java/BloomFilter.java
@@ -7,15 +7,20 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class BloomFilter {
     private List<String> Words = new ArrayList<>();     //stores all the words
     private String path;                                //path for words.txt
-    private Byte[] mBitArray = new Byte[100];           // Initial Byte Array with length 100
-    private int seed = 1996;                            //Seed for HashFunction
+    private byte [] mBitArray = new byte [1000000];  // Initial Bitset with length 100
+    private int seedOne = 1996;                            //Seed for HashFunction
+    private int seedTwo = 14;                            //Seed for HashFunction
+    private int sedThree = 4;                            //Seed for HashFunction
+    private int size = 1000000;
+    HashFunction hf1 = Hashing.murmur3_128(seedOne);
+    HashFunction hf2 = Hashing.murmur3_128(seedTwo);
+    HashFunction hf3 = Hashing.murmur3_128(sedThree);
+
 
     // ****************************CREATE A NEW BLOOM FILTER WITH WORDS FROM A EXTERNAL SOURCE ***********************************************************
     public BloomFilter(String path) throws IOException {
@@ -30,19 +35,50 @@ public class BloomFilter {
     }
     //Hash all the words in List
     public void HashAllWords(){
-        HashFunction hf = Hashing.murmur3_32(seed);
-        for(String s : Words){
-            HashCode code = hf.hashUnencodedChars(s);
-            int cod = code.asInt();
-            System.out.println(cod);
-        }
 
+        for(String s : Words){
+            int codeOne = hf1.hashUnencodedChars(s).asInt();
+            int codeTwo = hf2.hashUnencodedChars(s).asInt();
+            int codeThree = hf3.hashUnencodedChars(s).asInt();
+
+            codeOne = countModulo(codeOne);
+            codeTwo = countModulo(codeTwo);
+            codeThree = countModulo(codeThree);
+
+            FillIndex(codeOne, codeTwo, codeThree);
+        }
+        for(int i = 0; i<mBitArray.length;i++){
+            System.out.print(mBitArray[i]);
+        }
+        System.out.println();
+    }
+    public int countModulo(int input){
+        input = input % size;
+        if (input < 0) input *= -1;
+        return input;
+    }
+
+    public void FillIndex (int one, int two, int three){
+        mBitArray[one] = 1;
+        mBitArray[two] = 1;
+        mBitArray[three] = 1;
     }
 //Helper function to print all Words in List Words
     public void print() {
         for (String s : Words) {
             System.out.println(s);
         }
+    }
+    public boolean MayContain(String s){
+        int codeOne = hf1.hashUnencodedChars(s).asInt();
+        int codeTwo = hf2.hashUnencodedChars(s).asInt();
+        int codeThree = hf3.hashUnencodedChars(s).asInt();
+
+        codeOne = countModulo(codeOne);
+        codeTwo = countModulo(codeTwo);
+        codeThree = countModulo(codeThree);
+        if(mBitArray[codeOne] == 1 && mBitArray[codeTwo] == 1 && mBitArray[codeThree] == 1) return true;
+        return false;
     }
 
 
--- JavaProject/src/main/java/main.java
+++ JavaProject/src/main/java/main.java
@@ -2,9 +2,11 @@ import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-    BloomFilter bloom = new BloomFilter("TestData/words.txt");
-    bloom.print();
-    bloom.HashAllWords();
+        BloomFilter bloom = new BloomFilter("TestData/words.txt");
+        bloom.print();
+        bloom.HashAllWords();
+        System.out.println("Start Tests");
+        System.out.println(bloom.MayContain("Hello"));
     }
-    }
+}
 
